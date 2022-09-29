e2408f57edc8659436daa2076053c0af7feb4408
Sergei Zimmerman <serjtsimmerman@gmail.com>
2022-09-29 03:46:19 +0300

    Early version of vector

--- 04-linmath/01-hwmx/lib/include/vector.hpp
+++ 04-linmath/01-hwmx/lib/include/vector.hpp
@@ -10,35 +10,90 @@
 
 #pragma once
 
-#include <cstddef>
-#include <type_traits>
 #include <algorithm>
+#include <cstddef>
 #include <memory>
+#include <type_traits>
+#include <stdexcept>
 
 namespace throttle {
 namespace containers {
 
 template <typename T>
-requires std::is_nothrow_move_assignable_v<T>
+requires std::is_nothrow_move_assignable_v<T> && std::is_nothrow_destructible_v<T>
 class vector {
-  private:
-  T *m_buffer_ptr;
-  T *m_past_capacity_ptr;
-  T *m_past_end_ptr;
+private:
+  T *m_buffer_ptr = nullptr;
+  T *m_past_capacity_ptr = nullptr;
+  T *m_past_end_ptr = nullptr;
 
-  static constexpr std::size_t default_size = 128;
+  static constexpr std::size_t default_capacity = 128;
 
 public:
-  vector(std::size_t capacity = default_size)
-      : m_buffer_ptr{static_cast<T *>(::operator new(sizeof(T) * capacity))}, m_past_capacity_ptr{m_buffer_ptr + capacity},
-        m_past_end_ptr{m_buffer_ptr} {}
-  
-  ~vector() {
-    if constexpr (!std::is_trivially_destructible_v<T>) {
+  using size_type = std::size_t;
+  using value_type = T;
+
+private:
+  using pointer = value_type *;
+  using reference = value_type &;
+
+  void delete_elements() noexcept {
+    if constexpr (!std::is_trivially_destructible_v<value_type>) {
       std::destroy(m_buffer_ptr, m_past_end_ptr);
     }
-    ::operator delete(m_buffer_ptr);
+    m_past_end_ptr = m_buffer_ptr;
   }
+
+public:
+  vector(size_type capacity = default_capacity)
+      : m_buffer_ptr{static_cast<pointer>(::operator new(sizeof(value_type) * std::max(capacity, default_capacity)))},
+        m_past_capacity_ptr{m_buffer_ptr + std::max(capacity, default_capacity)}, m_past_end_ptr{m_buffer_ptr} {}
+
+  ~vector() {
+    auto deleter = [](pointer ptr) { ::operator delete(ptr); };
+
+    std::unique_ptr<value_type, decltype(deleter)> uptr{m_buffer_ptr, deleter};
+    delete_elements();
+  }
+
+  vector(vector &&rhs) noexcept {
+    std::swap(m_buffer_ptr, rhs.m_buffer_ptr);
+    std::swap(m_past_capacity_ptr, rhs.m_past_capacity_ptr);
+    std::swap(m_past_end_ptr, rhs.m_past_end_ptr);
+  }
+
+  vector &operator=(vector &&rhs) noexcept {
+    if (this == std::addressof(rhs)) return *this;
+    std::swap(m_buffer_ptr, rhs.m_buffer_ptr);
+    std::swap(m_past_capacity_ptr, rhs.m_past_capacity_ptr);
+    std::swap(m_past_end_ptr, rhs.m_past_end_ptr);
+    return *this;
+  }
+
+private:
+  void resize_if_necessary() {
+    if (m_past_capacity_ptr == m_past_end_ptr) throw std::runtime_error{"Not yet implemented resize"};
+  }
+
+public:
+  void push_back(const value_type &val) {
+    resize_if_necessary();
+    new (m_past_end_ptr++) T{val};
+  }
+
+  void push_back(value_type &&val) {
+    resize_if_necessary();
+    new (m_past_end_ptr++) T{std::move(val)};
+  }
+
+  void clear() { delete_elements(); }
+  void pop_back() { std::destroy_at(--m_past_end_ptr); }
+
+  size_type size() const noexcept { return m_past_end_ptr - m_buffer_ptr; }
+  size_type capacity() const noexcept { return m_past_capacity_ptr - m_buffer_ptr; }
+
+  value_type &operator[](size_type index) { return *(m_buffer_ptr + index); }
+  const value_type &operator[](size_type index) const { return *(m_buffer_ptr + index); }
 };
 
 } // namespace containers
--- 04-linmath/01-hwmx/test/determinant/src/determinant.cc
+++ 04-linmath/01-hwmx/test/determinant/src/determinant.cc
@@ -12,5 +12,21 @@ namespace po = boost::program_options;
 #endif
 
 int main(int argc, char *argv[]) {
-  throttle::containers::vector<int> vec;
+  throttle::containers::vector<std::unique_ptr<int>> vec;
+
+  for (int i = 0; i < 16; ++i) {
+    vec.push_back(std::make_unique<int>(i));
+  }
+
+  for (int i = 0; i < vec.size(); ++i) {
+    std::cout << *vec[i] << " ";
+  }
+  std::cout << "\n";
+
+  auto vec_moved = std::move(vec);
+
+  for (int i = 0; i < vec_moved.size(); ++i) {
+    std::cout << *vec_moved[i] << " ";
+  }
+  std::cout << "\n";
 }
